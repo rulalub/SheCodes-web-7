3f7f4699a55b01b21493c994231b93ec642462fc
rulalub <rulalub@hotmail.com>
2022-09-19 02:03:12 +0300

    changed  d with D in let now

--- script.js
+++ script.js
@@ -3,38 +3,52 @@ let apiKey=" aa93cf045af7ef6b96d7f0bf356e88c3"
 let apiUrl ="https://api.openweathermap.org/data/2.5/weather?q=New york&appid=$a{apiKey}"
 
 
-let now = new date ();
+let now = new Date ();
 let h1 =document.querySelector("h1");
 let hours= now.getHours();
-let minutes = now.get.Minutes();
+let minutes = now.getMinutes();
 
 
-let days = days[now.getDay()];
-h1.innerHTML= '${day}, ${hours}: ${minutes}';
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let day = days[now.getDay()];
+h1.innerHTML = `${day}, ${hours}: ${minutes}`;
 
 
-function citySearch(response) {
-    console.log(response.data);
-    let temperatureElement= document.querySelector("#temperature");
-    temperatureElement.innerHTML= `$"{response.data.main}`
+function citySearch(city) {
   event.preventDefault();
   let apiKey = "aa93cf045af7ef6b96d7f0bf356e88c3";
   let units = "metric";
-  let city = document.querySelector("#input-form").value;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(showWeather); 
+  axios.get(apiUrl).then(showWeather);
+}
+
+function showWeather(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#unit-temperature");
+  temperatureElement.innerHTML = `${response.data.main.temp}`;
 }
 
 
-function signUp(event) {
+function search(event) {
   event.preventDefault();
-  let input = document.querySelector("#email");
-  console.log(input.value);
+  let input = document.querySelector(".city");
+  citySearch(input.value);
 }
-
 
 let form = document.querySelector("#search-form");
-form.addEventListener("submit", citySearch);
+form.addEventListener("submit", search);
+
+
+
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
   celciusTemp.classList.remove("active");
@@ -44,7 +58,7 @@ function displayFahrenheitTemperature(event) {
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
   celciusTemperature = response.data.main.temp;
 }
-let fahreneitTemp = document.querySelector("#fahrenheit-temp");
+let fahreneitTemp = document.querySelector("#fahrenheit-link");
 fahreneitTemp.addEventListener("click", displayFahrenheitTemperature);
 function displayCelciusTemperature(event) {
   event.preventDefault();
@@ -54,6 +68,6 @@ function displayCelciusTemperature(event) {
   temperatureElement.innerHTML = Math.round(celciusTemperature);
 }
 let celciusTemperature = null;
-let celciusTemp = document.querySelector("#celcius-temp");
+let celciusTemp = document.querySelector("#celcius-link");
 celciusTemp.addEventListener("click", displayCelciusTemperature);
 
